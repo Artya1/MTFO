3094d57c1620661d35b767ca170425338424c4b3
Tsuperion <160936055+Tsuperion@users.noreply.github.com>
2025-06-25 18:13:40 -0400

    started things
    added things

---
+++ Assets/Scripts/ObjectScripts/PickupObject.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PickupObject : MonoBehaviour, IObject
+{
+    public GameObject GameObject => gameObject;
+
+    public void OnPickup()
+    {
+        // Example: Disable renderer or change color when picked up
+        Debug.Log($"{gameObject.name} was picked up!");
+    }
+
+    public void OnDrop()
+    {
+        // Example: Re-enable renderer or reset state when dropped
+        Debug.Log($"{gameObject.name} was dropped!");
+    }
+}
